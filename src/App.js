6572eb0d874897b48e2ba14948a526239bf54ce7
mohitkumar54534 <mohitkumar54534@gmail.com>
2023-10-20 22:35:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// src/App.js
+import React from 'react';
+import SearchBar from './Components/SearchBar';
+import './App.css';
+import './Components/SearchBar.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar />
+      {/* Add the rest of your application components here */}
+    </div>
+  );
+}
+
+export default App;
